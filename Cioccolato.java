d4d045f6d7051f4e6648094dee37627fe8286565
Di Zenzo Carmine Federico <114579198+dizenzoc@users.noreply.github.com>
2024-12-12 17:35:16 +0100

    Update Cioccolato
    aggiunta prima parte menu

--- Cioccolato.java
+++ Cioccolato.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public abstract class Cioccolato {
 
     protected String tipoDiCioccolato;
@@ -64,7 +66,49 @@ public abstract class Cioccolato {
     public static void main(String[] args) {
         final int MAX_CIOCCOLATO_GIORNALIERO = 100;
         int cioccolatoRimasto = MAX_CIOCCOLATO_GIORNALIERO;
+
+        Scanner scInt = new Scanner(System.in); // Scanner per input interi
+        boolean menu = true; //booleano per mostrare menu
         
-        boolean continuare = true;
+        while(menu) {
+            mostraMenu();
+            int scelta = scInt.nextInt();
+            scInt.nextLine(); // Consuma la newline
+            switch (scelta) {
+                case 1:
+                    System.out.println();
+                    //logica per cioccolatino
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println();
+                    //logica per tavoletta
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println();
+                    //logica per cioccolata calda
+                    System.out.println();
+                    break;
+                case 4:
+                    menu = false;
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("Scelta non valida. Riprova.");
+                    System.out.println();
+            }
+
+
+        }
+    }
+
+    private static void mostraMenu() {
+        System.out.println("Cosa vuoi aggiungere?");
+        System.out.println("1. Cioccolatino");
+        System.out.println("2. Tavoletta");
+        System.out.println("3. Cioccolata Calda");
+        System.out.println("4. Esci");
+        System.out.print("Scegli un'opzione: ");
     }
 }
