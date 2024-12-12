a9ad919259928cf45e183aa0b4b6d779616059c0
Di Zenzo Carmine Federico <114579198+dizenzoc@users.noreply.github.com>
2024-12-12 17:05:06 +0100

    Update Cioccolato
    variabili - costruttore - metodi get/set/produce

--- Cioccolato.java
+++ Cioccolato.java
@@ -1,3 +1,64 @@
-public class abstract Cioccolato {
-		
+public abstract class Cioccolato {
+
+    protected String tipoDiCioccolato;
+    protected String tipoDiAggiunta;
+    protected int percentualeCacao;
+    protected int unita;
+
+    // costruttore vuoto
+    public Cioccolato() {
+
+    }
+
+    //costruttore
+    public Cioccolato(String tipoDiCioccolato, String tipoDiAggiunta, int percentualeCacao, int unita) {
+        this.tipoDiCioccolato = tipoDiCioccolato;
+        this.tipoDiAggiunta = tipoDiAggiunta;
+        this.percentualeCacao = percentualeCacao;
+        this.unita = unita;
+    }
+
+    // getter e setter
+    public String getTipoDiCioccolato() {
+        return tipoDiCioccolato;
+    }
+
+    public void setTipoDiCioccolato(String tipoDiCioccolato) {
+        this.tipoDiCioccolato = tipoDiCioccolato;
+    }
+
+    public String getTipoDiAggiunta() {
+        return tipoDiAggiunta;
+    }
+
+    public void setTipoDiAggiunta(String tipoDiAggiunta) {
+        this.tipoDiAggiunta = tipoDiAggiunta;
+    }
+
+    public int getPercentualeCacao() {
+        return percentualeCacao;
+    }
+
+    public void setPercentualeCacao(int percentualeCacao) {
+        this.percentualeCacao = percentualeCacao;
+    }
+
+    public int getUnita() {
+        return unita;
+    }
+
+    public void setUnita(int unita) {
+        this.unita = unita;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Cioccolato{" +
+                "tipoDiCioccolato='" + tipoDiCioccolato + '\'' +
+                ", tipoDiAggiunta='" + tipoDiAggiunta + '\'' +
+                ", percentualeCacao=" + percentualeCacao +
+                ", unita=" + unita +
+                '}';
+    }
 }
