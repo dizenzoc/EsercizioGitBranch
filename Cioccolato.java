a548c9458426b4cef57273a84970af154116cf3a
Di Zenzo Carmine Federico <114579198+dizenzoc@users.noreply.github.com>
2024-12-13 11:45:54 +0100

    Update Cioccolato
    aggiunta del metodo creaCioccolatino per il menu principale.

--- Cioccolato.java
+++ Cioccolato.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public abstract class Cioccolato {
@@ -56,10 +57,10 @@ public abstract class Cioccolato {
     // toString
     public void produce() {
         System.out.println("Cioccolato{" +
-                "tipoDiCioccolato='" + tipoDiCioccolato + '\'' +
-                ", tipoDiAggiunta='" + tipoDiAggiunta + '\'' +
-                ", percentualeCacao=" + percentualeCacao +
-                ", unita=" + unita +
+                "\n\ttipoDiCioccolato='" + tipoDiCioccolato + '\'' +
+                "\n\ttipoDiAggiunta='" + tipoDiAggiunta + '\'' +
+                "\n\tpercentualeCacao=" + percentualeCacao +
+                "\n\tunita=" + unita +
                 '}');
     }
 
@@ -68,7 +69,9 @@ public abstract class Cioccolato {
         int cioccolatoRimasto = MAX_CIOCCOLATO_GIORNALIERO;
 
         Scanner scInt = new Scanner(System.in); // Scanner per input interi
+        //Scanner scStr = new Scanner(System.in);
         boolean menu = true; //booleano per mostrare menu
+        ArrayList<Cioccolato> lista = new ArrayList<>();
         
         while(menu) {
             mostraMenu();
@@ -76,9 +79,23 @@ public abstract class Cioccolato {
             scInt.nextLine(); // Consuma la newline
             switch (scelta) {
                 case 1:
-                    System.out.println();
-                    //logica per cioccolatino
-                    System.out.println();
+                    if (cioccolatoRimasto >= 2) {
+                        lista.add(creaCioccolatino(scInt));
+                        cioccolatoRimasto -= 2;
+                    }
+                    else{
+                        break;
+                        // System.out.println("Per oggi abbiamo finito... Vuoi continuare?");
+                        // int risp = 0;
+                        // switch (risp) {
+                        //     case 1:
+                        //         cioccolatoRimasto = MAX_CIOCCOLATO_GIORNALIERO;
+                        //         break;
+                        
+                        //     default:
+                        //         break;
+                        // }
+                    }
                     break;
                 case 2:
                     System.out.println();
@@ -98,8 +115,6 @@ public abstract class Cioccolato {
                     System.out.println("Scelta non valida. Riprova.");
                     System.out.println();
             }
-
-
         }
     }
 
@@ -110,5 +125,147 @@ public abstract class Cioccolato {
         System.out.println("3. Cioccolata Calda");
         System.out.println("4. Esci");
         System.out.print("Scegli un'opzione: ");
+    }
+
+    private static void mostraMenuTipoCioccolatino() {
+        System.out.println("Tipo di cioccolato:");
+        System.out.println("1. Al Latte");
+        System.out.println("2. Fondente");
+        System.out.print("Scegli un'opzione: ");
+    }
+
+    private static void mostraMenuAggiuntaCioccolatino() {
+        System.out.println("Tipo di aggiunta:");
+        System.out.println("1. Nocciole");
+        System.out.println("2. Mandorle");
+        System.out.println("3. Oreo");
+        System.out.print("Scegli un'opzione: ");
+    }
+
+    private static void mostraMenuFormaCioccolatino() {
+        System.out.println("Forma Cioccolatino:");
+        System.out.println("1. Rettangolo");
+        System.out.println("2. Quadrato");
+        System.out.print("Scegli un'opzione: ");
+    }
+
+    private static void mostraMenuRipienoCioccolatino() {
+        System.out.println("Tipo di aggiunta:");
+        System.out.println("1. Crema");
+        System.out.println("2. Nutella");
+        System.out.print("Scegli un'opzione: ");
+    }
+
+    private static Cioccolatino creaCioccolatino(Scanner scInt){
+        Cioccolatino nuovoCioccolatino = new Cioccolatino();
+        nuovoCioccolatino.setUnita(2); //valore fisso
+        
+        boolean menu = true;
+        //menu per tipo di cioccolatino
+        while(menu){
+            mostraMenuTipoCioccolatino();
+            int scelta = scInt.nextInt();
+            scInt.nextLine(); // Consuma la newline
+            switch (scelta) {
+                case 1:
+                    nuovoCioccolatino.setTipoDiCioccolato("Al Latte");
+                    menu = false;
+                    break;
+                case 2:
+                    nuovoCioccolatino.setTipoDiCioccolato("Fondente");
+                    menu = false;
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("Scelta non valida. Riprova.");
+                    System.out.println();
+            }
+        }
+        menu = true;
+        //menu per tipo di aggiunta al cioccolatino
+        while(menu){
+            mostraMenuAggiuntaCioccolatino();
+            int scelta = scInt.nextInt();
+            scInt.nextLine(); // Consuma la newline
+            switch (scelta) {
+                case 1:
+                    nuovoCioccolatino.setTipoDiAggiunta("Nocciole");
+                    menu = false;
+                    break;
+                case 2:
+                    nuovoCioccolatino.setTipoDiAggiunta("Mandorle");
+                    menu = false;
+                    break;
+                case 3:
+                    nuovoCioccolatino.setTipoDiAggiunta("Oreo");
+                    menu = false;
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("Scelta non valida. Riprova.");
+                    System.out.println();
+            }
+        }
+        System.out.println("Inserisci percentuale cacao: ");
+        int percentuale = scInt.nextInt();
+        scInt.nextLine(); // Consuma la newline
+        nuovoCioccolatino.setPercentualeCacao(percentuale);
+
+        menu = true;
+        //menu per forma di cioccolatino
+        while(menu){
+            mostraMenuFormaCioccolatino();
+            int scelta = scInt.nextInt();
+            scInt.nextLine(); // Consuma la newline
+            switch (scelta) {
+                case 1:
+                    nuovoCioccolatino.setForma("Rettangolo");
+                    menu = false;
+                    break;
+                case 2:
+                    System.out.println();
+                    nuovoCioccolatino.setForma("Quadrato");
+                    menu = false;
+                    System.out.println();
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("Scelta non valida. Riprova.");
+                    System.out.println();
+            }
+        }
+        menu = true;
+        //menu per tipo di cioccolatino
+        while(menu){
+            mostraMenuRipienoCioccolatino();
+            int scelta = scInt.nextInt();
+            scInt.nextLine(); // Consuma la newline
+            switch (scelta) {
+                case 1:
+                    nuovoCioccolatino.setRipieno("Crema");
+                    menu = false;
+                    break;
+                case 2:
+                    nuovoCioccolatino.setRipieno("Nutella");
+                    menu = false;
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("Scelta non valida. Riprova.");
+                    System.out.println();
+            }
+        }
+        System.out.println("Nuovo cioccolatino creato");
+        nuovoCioccolatino.produce();
+        return nuovoCioccolatino;
+    }
+
+    private static Tavoletta creaTavoletta(){
+        
+        return null;
+    }
+
+    private static CioccolataCalda creaCioccolataCalda(){
+        return null;
     }
 }
