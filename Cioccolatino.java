1e7935298327b78cc4438777764f1d21ab12e5dc
Di Zenzo Carmine Federico <114579198+dizenzoc@users.noreply.github.com>
2024-12-13 11:45:18 +0100

    Update Cioccolatino
    modifica del metodo produce() e aggiunta del costruttore vuoto e dei metodi get and set.

--- Cioccolatino.java
+++ Cioccolatino.java
@@ -2,6 +2,10 @@ public class Cioccolatino extends Cioccolato{
     public String forma;
     public String ripieno;
 
+    public Cioccolatino(){
+
+    }
+
     public Cioccolatino(String tipoDiCioccolato, String tipoDiAggiunta, int percentualeCacao, int unita, String forma, String ripieno){
         super(tipoDiCioccolato, tipoDiAggiunta, percentualeCacao, unita);
         this.forma = forma;
@@ -10,6 +14,31 @@ public class Cioccolatino extends Cioccolato{
 
     @Override
     public void produce(){
-        System.out.println(super.toString() + "di forma: " + forma + "; ripieno di: " + ripieno);
+        System.out.println("Cioccolatino{\n" +
+                "\ttipoDiCioccolato='" + tipoDiCioccolato + '\'' +
+                "\n\ttipoDiAggiunta='" + tipoDiAggiunta + '\'' +
+                "\n\tpercentualeCacao=" + percentualeCacao +
+                "\n\tunita=" + unita +
+                "\n\tforma=" + forma +
+                "\n\tripieno=" + ripieno +
+                '}');
     }
+
+    public String getForma() {
+        return forma;
+    }
+
+    public void setForma(String forma) {
+        this.forma = forma;
+    }
+
+    public String getRipieno() {
+        return ripieno;
+    }
+
+    public void setRipieno(String ripieno) {
+        this.ripieno = ripieno;
+    }
+
+    
 }
